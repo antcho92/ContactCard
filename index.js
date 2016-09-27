1813b8e9a48281ff73c23df187054ee6a5081a95
Anthony <antcho92@gmail.com>
2016-09-27 09:51:10 -0700

    working contact card addition with description change

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    console.log($($textarea).val());
+    $(document).on("click", ".description", function() {
+        $(this).html($(this).attr("alt-text"));
+    });
+
+
+    $("form").submit(function() {
+        var firstName = $("#firstName").val(),
+            lastName = $("#lastName").val(),
+            description = $("#description").val();
+        var card ="<div class='card'><h2>" + firstName + " " + lastName + "</h2><p class='description' alt-text=" + description + ">Click for description!</p></div>";
+        $("#cards").append(card);
+        $(".card").css({
+            "border": "1px solid black",
+            "width": "250px",
+            "height": "auto"
+        });
+        return false;
+    });
 });
